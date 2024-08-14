b4bf6157389c9a669f957d33b9e6a90052bac622
lex banzon jr <lexbanzonjr@gmail.com>
2024-08-13 22:22:04 -0700

    Created RestError and refactored

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -2,6 +2,7 @@ import bcrpty from "bcrypt";
 import { Request, Response } from "express";
 import { responseReturn } from "../utils/response";
 import { createToken } from "../utils/createToken";
+import { RestError } from "../utils/RestError";
 
 const adminModel = require("../models/adminModel");
 
@@ -10,23 +11,32 @@ class authController {
     const { email, password } = req.body;
     try {
       const admin = await adminModel.findOne({ email }).select("+password");
-      const match = await bcrpty.compare(password, admin.password);
-      if (admin && match) {
-        const token = createToken({
-          id: admin.id,
-          role: admin.role,
-        });
-        res.cookie("accessToken", token, {
-          expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-        });
-        responseReturn(res, 200, "Login successful");
-      } else {
-        responseReturn(res, 400, {
-          error: "Email and/or password not not valid",
+      if (!admin) {
+        throw new RestError("Email and/or password not not valid", {
+          status: 400,
         });
       }
+      const match = await bcrpty.compare(password, admin.password);
+      if (!match) {
+        throw new RestError("Email and/or password not not valid", {
+          status: 400,
+        });
+      }
+
+      const token = createToken({
+        id: admin.id,
+        role: admin.role,
+      });
+      res.cookie("accessToken", token, {
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      });
+
+      responseReturn(res, 200, {
+        message: "Login successful",
+        accessToken: token,
+      });
     } catch (error: any) {
-      responseReturn(res, 500, { error: error.message });
+      responseReturn(res, error.status || 500, { error: error.message });
     }
     next();
   };
