c9d14ff2102a6e1fb76b4d4891f31c8f508c64ba
lex banzon jr <lexbanzonjr@gmail.com>
2024-11-07 19:06:03 -0800

    Implemented yahoo tokenMiddleware

---
+++ middlewares/yahoo/tokenMiddleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { decodeToken } from "../../utils/token";
+import userModel from "../../models/yahoo/userModel";
+import { RestError } from "../../utils/RestError";
+import { now } from "mongoose";
+
+export const tokenMiddleware = async (
+  req: Request,
+  res: Response,
+  next: any
+) => {
+  const { accessToken } = req.cookies;
+  if (!accessToken) {
+    return res.status(409).json({ error: "Please login." });
+  }
+
+  try {
+    const data = decodeToken(accessToken);
+    const user = await userModel.findOne({ userId: data.id });
+    if (null === user) throw new RestError("Please login.", { status: 400 });
+    if (new Date() > user.tokenExpire) {
+      console.log("token expired, refreshing token");
+      return res.redirect(
+        "/yahoo/auth/refresh_token?redirect=" + req.originalUrl
+      );
+    }
+    next();
+  } catch (error) {
+    return res.status(409).json({ error });
+  }
+};
