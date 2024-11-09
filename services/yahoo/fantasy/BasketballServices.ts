b23074f62b1750afd6831d938ddc4b024ac8b033
lex banzon jr <lexbanzonjr@gmail.com>
2024-11-09 10:44:41 -0800

    refactor: cleanup

--- services/yahoo/fantasy/BasketballServices.ts
+++ services/yahoo/fantasy/BasketballServices.ts
@@ -1,12 +1,23 @@
 import axios from "axios";
 import { parseStringPromise } from "xml2js";
 
+export interface Player {
+  player_key: string;
+  name: string;
+  status: string[];
+  primary_position: string;
+  positions: string[];
+}
+
+export interface Team {
+  team_key: string;
+  team_id: string;
+  name: string;
+}
+
 class BasketballServices {
-  roster = async (params: {
-    access_token: string;
-    team_key: string | undefined;
-  }) => {
-    if (params.team_key === undefined) throw Error("team_key undefined");
+  roster = async (params: { access_token: string; team_key?: string }) => {
+    if (params.team_key === undefined) throw Error("team_key required");
     const response = await axios.get(
       `https://fantasysports.yahooapis.com/fantasy/v2/team/${params.team_key}/roster`,
       {
@@ -17,13 +28,8 @@ class BasketballServices {
     );
     const json: any = await parseStringPromise(response.data);
 
-    let players: {
-      player_key: string;
-      name: string;
-      status: string[];
-      primary_position: string;
-      positions: string[];
-    }[] = [];
+    // List players
+    let players: Player[] = [];
     json.fantasy_content.team[0].roster[0].players[0].player.forEach(
       (player: any) => {
         players.push({
@@ -44,6 +50,7 @@ class BasketballServices {
     team_key?: string;
     name?: string;
   }) => {
+    const teams: Team[] = [];
     const response = await axios.get(
       "https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=nba/teams",
       {
@@ -53,32 +60,36 @@ class BasketballServices {
       }
     );
 
-    const json = await parseStringPromise(response.data);
+    // xml to json
+    const jsonResponse = await parseStringPromise(response.data);
+    interface jsonTeam {
+      team_key: string[];
+      team_id: string[];
+      name: string[];
+    }
+    const jsonTeams: jsonTeam[] =
+      jsonResponse.fantasy_content.users[0].user[0].games[0].game[0].teams[0]
+        .team;
 
-    let result: { team_key: string; team_id: string; name: string }[] = [];
-    const teams: { team_key: string[]; team_id: string[]; name: string[] }[] =
-      json.fantasy_content.users[0].user[0].games[0].game[0].teams[0].team;
+    // iterate each team from the response
+    jsonTeams.forEach((jsoTeams) => {
+      const team_key = jsoTeams.team_key[0];
+      const name = jsoTeams.name[0];
 
-    teams.forEach((team) => {
-      let push = false;
-
-      if (params.team_key !== undefined || params.name !== undefined) {
-        push =
-          team.team_key[0] === params.team_key || team.name[0] === params.name;
-      } else {
-        push = true;
-      }
-
-      if (push) {
-        result.push({
-          team_key: team.team_key[0],
-          team_id: team.team_id[0],
-          name: team.name[0],
+      // add to the list
+      const paramExists = params.team_key || params.name;
+      const doesOneParamMatch =
+        team_key === params.team_key || name === params.name;
+      if (paramExists ? doesOneParamMatch : true) {
+        teams.push({
+          team_key: jsoTeams.team_key[0],
+          team_id: jsoTeams.team_id[0],
+          name: jsoTeams.name[0],
         });
       }
     });
 
-    return result;
+    return { teams };
   };
 }
 
--- services/yahoo/fantasy/FantasyServices.ts
+++ services/yahoo/fantasy/FantasyServices.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 import { parseStringPromise } from "xml2js";
 
+export interface League {
+  league_key: string;
+  name: string;
+}
 class FantasyServices {
   league = async (params: { access_token: string; name?: string }) => {
+    let leagues: League[] = [];
     const response = await axios.get(
       "https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games/leagues",
       {
@@ -11,24 +16,40 @@ class FantasyServices {
         },
       }
     );
-    const json: any = await parseStringPromise(response.data);
+    const jsonResponse = await parseStringPromise(response.data);
+    interface jsonGame {
+      leagues?: jsonLeague[];
+    }
+    interface jsonLeague {
+      league: jsonLeagueDetails[];
+    }
+    interface jsonLeagueDetails {
+      league_key: string;
+      name: string;
+    }
+    const jsonGames: jsonGame[] =
+      jsonResponse.fantasy_content.users[0].user[0].games[0].game;
 
-    const games = json.fantasy_content.users[0].user[0].games[0].game;
-    let result;
-    games.find((game: any) => {
-      if (game.leagues === undefined) return;
-      return game.leagues.find((league: any) => {
-        if (league.league[0].name[0] === params.name) {
-          result = {
-            league_key: league.league[0].league_key[0],
-            name: league.league[0].name[0],
-          };
-          return;
+    // Loop through each league you've ever been on
+    jsonGames.forEach((game) => {
+      const jsonLeagues = game.leagues;
+
+      if (jsonLeagues === undefined) return;
+
+      jsonLeagues.forEach((jsonLeague) => {
+        const league_key = jsonLeague.league[0].league_key[0];
+        const name = jsonLeague.league[0].name[0];
+
+        // add to the list
+        const paramExists = params.name;
+        const doesOneParamMatch = name === params.name;
+        if (paramExists ? doesOneParamMatch : true) {
+          leagues.push({ league_key, name });
         }
       });
     });
 
-    return result;
+    return { leagues };
   };
 }
 
