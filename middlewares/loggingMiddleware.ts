c811f8e5bf421a8e1c6d895289dcd9f2314d2b1a
lex banzon jr <lexbanzonjr@gmail.com>
2024-11-25 00:14:05 -0800

    Log requests

--- middlewares/loggingMiddleware.ts
+++ middlewares/loggingMiddleware.ts
@@ -1,31 +1,16 @@
 import { Request, Response } from "express";
-import { decodeToken } from "../utils/token";
-import userModel from "../models/userModel";
 
 export const loggingMiddleware = async (
   req: Request,
   res: Response,
   next: any
 ) => {
-  const { accessToken } = req.cookies;
+  const log = {
+    request_api: `${req.method} ${req.originalUrl}`,
+    request_body: JSON.stringify(req.body),
+    // request_headers: JSON.stringify(req.headers),
+  };
 
-  if (accessToken) {
-    try {
-      const data = decodeToken(accessToken);
-
-      const user = await userModel.findById(data.id);
-      const username = user?.name ?? "unknown";
-
-      const log = {
-        username,
-        request_api: `${req.method} ${req.originalUrl}`,
-        request_body: JSON.stringify(req.body),
-      };
-
-      console.log(log);
-    } catch (error) {
-      return res.status(409).json({ error });
-    }
-  }
+  console.log(log);
   next();
 };
