3adee7ef56b7274698159fe64985f0417b6dca7d
lex banzon jr <lexbanzonjr@gmail.com>
2025-02-01 14:38:16 -0800

    Remove createModelOverride from CategoryController and ProductController.ts

--- controllers/CategoryController.ts
+++ controllers/CategoryController.ts
@@ -5,19 +5,6 @@ class CategoryController extends BaseController<ICategory> {
   constructor() {
     super({
       model: categoryModel,
-      createModelOverride: async (props: ICategory) => {
-        const category = new categoryModel(props);
-        if (category.name === "") {
-          let int = 0;
-          category.name = "category" + int;
-          while (
-            null !== (await categoryModel.exists({ name: category.name }))
-          ) {
-            category.name = "category" + int++;
-          }
-        }
-        return category;
-      },
     });
   }
 }
--- controllers/ProductController.ts
+++ controllers/ProductController.ts
@@ -5,19 +5,6 @@ class ProductController extends BaseController<IProduct> {
   constructor() {
     super({
       model: productModel,
-      createModelOverride: async (props: IProduct) => {
-        delete props.category_id;
-        const product = new productModel(props);
-        console.log(product);
-        if (product.name === "") {
-          let int = 0;
-          product.name = "product" + int;
-          while (null !== (await productModel.exists({ name: product.name }))) {
-            product.name = "product" + int++;
-          }
-        }
-        return product;
-      },
     });
   }
 }
