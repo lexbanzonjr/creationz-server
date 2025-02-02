c47b7c0888173e4233b18fa29aaf79135428742e
lex banzon jr <lexbanzonjr@gmail.com>
2025-02-01 16:14:17 -0800

    Add multer to package.json to support upload of binaries

--- routes/binaryRoute.ts
+++ routes/binaryRoute.ts
@@ -3,7 +3,12 @@ import multer from "multer";
 
 import BinaryController from "../controllers/BinaryController";
 
+// Multer setup
+const storage = multer.memoryStorage(); // Store file in memory
+const upload = multer({ storage });
+
 let router = Router();
 router.get("/:_id", BinaryController.get);
+router.post("", upload.single("binary"), BinaryController.create);
 
 module.exports = router;
