d0f6896e5e67c165de31b82cd73ca20af736a2ec
lex banzon jr <lexbanzonjr@gmail.com>
2025-06-21 17:37:41 -0700

    Implement create for category and product

--- handlers/CategoryHandler.ts
+++ handlers/CategoryHandler.ts
@@ -1,9 +1,17 @@
+import { Request, Response } from "express";
 import categoryModel, { ICategory } from "../models/categoryModel";
 import BaseHandler from "./BaseHandler";
 
 class CategoryHandler extends BaseHandler<ICategory> {
   constructor() {
     super({ model: categoryModel });
+  }
+
+  async create(req: Request, res: Response, next: any) {
+    const { name, description } = req.body;
+    const category = new categoryModel({ name, description });
+    await category.save();
+    res.status(201).json({ category });
   }
 }
 
--- handlers/ProductHandler.ts
+++ handlers/ProductHandler.ts
@@ -1,9 +1,26 @@
+import { Request, Response } from "express";
 import productModel, { IProduct } from "../models/productModel";
 import BaseHandler from "./BaseHandler";
+import { sendJsonResponse } from "../utils/response";
 
 class ProductHandler extends BaseHandler<IProduct> {
   constructor() {
     super({ model: productModel });
+  }
+
+  async create(req: Request, res: Response, next: any) {
+    const { name, description, cost, category_id, image_id, materials } =
+      req.body;
+    const product = new productModel({
+      name,
+      description,
+      cost,
+      category_id,
+      image_id,
+      materials,
+    });
+    await product.save();
+    sendJsonResponse(res, 200, { [productModel.modelName]: product });
   }
 }
 
