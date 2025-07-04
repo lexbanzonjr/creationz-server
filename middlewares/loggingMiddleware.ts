62ecbd35b3663e73be3d4d1f3dceb0d6057fea26
lex banzon jr <lexbanzonjr@gmail.com>
2025-07-03 18:13:33 -0700

    Remove the try from decodeToken and let the caller handle the exception

--- middlewares/loggingMiddleware.ts
+++ middlewares/loggingMiddleware.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from "express";
+import { decodeToken } from "../utils/token";
+import { TokenExpiredError } from "jsonwebtoken";
 
 export const loggingMiddleware = async (
   req: Request,
   res: Response,
   next: any
 ) => {
+  const authHeader = req.headers["authorization"];
+  if (authHeader) {
+    // Extract the Base64 encoded part (after "Basic ")
+    try {
+      const token = decodeToken(authHeader.split(" ")[1]);
+    } catch (error: any) {
+      if (error instanceof TokenExpiredError) {
+        res.status(401).send("Token expired");
+      }
+    }
+  }
   const log = {
     request_api: `${req.method} ${req.originalUrl}`,
     request_body: JSON.stringify(req.body),
--- utils/token.ts
+++ utils/token.ts
@@ -1,20 +1,12 @@
 import jwt, { Secret, sign } from "jsonwebtoken";
 import { IAccessTokenData, IGuestTokenData } from "./types";
-import { RestError } from "./RestError";
 
 export const createToken = (data: IAccessTokenData | IGuestTokenData) => {
   return sign(data, process.env.SECRET as Secret, { expiresIn: "7d" });
 };
 
-export const decodeToken = (token: string, throwOnError: boolean) => {
-  try {
-    return jwt.verify(token, process.env.SECRET as Secret) as
-      | IAccessTokenData
-      | IGuestTokenData;
-  } catch (error) {
-    if (throwOnError) {
-      throw new RestError("Invalid token", { status: 401 });
-    }
-    return null;
-  }
+export const decodeToken = (token: string) => {
+  return jwt.verify(token, process.env.SECRET as Secret) as
+    | IAccessTokenData
+    | IGuestTokenData;
 };
