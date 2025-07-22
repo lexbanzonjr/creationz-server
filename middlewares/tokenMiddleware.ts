8d54ba3e43d6372dbd369dc60baeff4627a0d3db
lex banzon jr <lexbanzonjr@gmail.com>
2025-07-22 07:13:43 -0700

    Return 401 for missing or invalid tokens

--- middlewares/tokenMiddleware.ts
+++ middlewares/tokenMiddleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { TokenExpiredError } from "jsonwebtoken";
 import { decodeToken } from "../utils/token";
+import { sendJsonResponse } from "../utils/response";
 
 export const tokenMiddleware = async (
   req: Request,
@@ -10,10 +11,16 @@ export const tokenMiddleware = async (
   res.locals = { populate: req.query.populate };
 
   const authHeader = req.headers["authorization"];
-  if (!authHeader) return next();
+  if (!authHeader)
+    return sendJsonResponse(res, 401, {
+      error: "No authorization header provided",
+    });
 
   const [authType, token] = authHeader.split(" ");
-  if (authType !== "Bearer" || !token) return next();
+  if (authType !== "Bearer" || !token)
+    return sendJsonResponse(res, 401, {
+      error: "Invalid authorization format",
+    });
 
   try {
     res.locals.token = token;
