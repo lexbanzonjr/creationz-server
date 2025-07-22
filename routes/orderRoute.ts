349cd630a80db90dbf9e9c8c992fe01b69634e98
lex banzon jr <lexbanzonjr@gmail.com>
2025-07-22 07:58:14 -0700

    Use tokenMiddleware for order routes

--- routes/orderRoute.ts
+++ routes/orderRoute.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import OrderHandler from "../handlers/OrderHandler";
+import { cartMiddleware } from "../middlewares/cartMiddleware";
 
 let router = Router();
+router.use(cartMiddleware);
 router.post("/", OrderHandler.create);
 router.get("/", OrderHandler.list);
 router.get("/:order_id", OrderHandler.getById);
